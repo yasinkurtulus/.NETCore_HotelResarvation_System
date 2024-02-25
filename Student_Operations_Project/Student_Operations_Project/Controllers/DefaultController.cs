06b67ceba0605008702de18f146bff5a3512d04c
Yasin <kurtulusyasin78@gmail.com>
2024-02-25 20:09:01 +0300

    First Commit

---
+++ Student_Operations_Project/Student_Operations_Project/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Operations_Project.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Student_Operations_Project/Student_Operations_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Student_Operations_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
